1de8e4f13668f30137f13f7e2a7a90970b65a08d
Marco Oliveira <me@marcooliveira.com>
2015-07-15 11:29:15 +0100

    [upd] initial implmentation of tests - 92% coverage

--- test/specs/replace.js
+++ test/specs/replace.js
@@ -9,182 +9,145 @@ module.exports = function () {
     db = db.get();
 
     it('should replace single key', function (done) {
-        // CONTINUE
-
         db.insert({
             some_key: 'now exists'
-        }, function (err, existing) {
+        }, function (err, misses) {
             __throw(err);
 
-            expect(existing).to.not.contain('some_key1');
-            expect(existing.length).to.be(0);
+            expect(misses).to.not.contain('some_key1');
+            expect(misses.length).to.be(0);
 
-            db.get('some_key', function (err, res) {
+            db.replace({
+                some_key: 'has been replaced'
+            }, function (err) {
                 __throw(err);
 
-                expect(res).to.be('now exists');
+                db.get('some_key', function (err, res) {
+                    __throw(err);
 
-                return done();
+                    expect(res).to.be('has been replaced');
+
+                    return done();
+                });
             });
         });
     });
 
-    it('should insert multiple keys', function (done) {
+    it('should replace multiple keys', function (done) {
         db.insert({
             some_key1: 'now exists1',
             some_key2: 'now exists2'
-        }, function (err, existing) {
+        }, function (err, misses) {
             __throw(err);
 
-            expect(existing).to.not.contain('some_key1');
-            expect(existing).to.not.contain('some_key2');
-            expect(existing.length).to.be(0);
+            expect(misses).to.not.contain('some_key1');
+            expect(misses).to.not.contain('some_key2');
+            expect(misses.length).to.be(0);
 
-            var tasks = {};
-
-            tasks.some_key1 = db.get.bind(db, 'some_key1');
-            tasks.some_key2 = db.get.bind(db, 'some_key2');
-
-            async.parallel(tasks, function (err, res) {
-                expect(res.some_key1).to.be('now exists1');
-                expect(res.some_key2).to.be('now exists2');
-            });
-
-            return done();
-        });
-    });
-
-    it('should be atomic', function (done) {
-        var tasks = {};
-
-        var total = 1000;
-
-        for (var i = 0; i < total; i++) {
-            tasks[i] = db.insert.bind(db, {
-                'some_key': i
-            });
-        }
-
-        async.parallel(tasks, function (err, res) {
-            __throw(err);
-
-            var totalExisting = 0,
-                totalInserted = 0,
-                valueInserted;
-
-            for (var k in res) {
-                if (res[k].length) {
-                    totalExisting += res[k].length;
-                } else {
-                    totalInserted++;
-                    valueInserted = parseInt(k, 10);
-                }
-            }
-
-            expect(totalExisting).to.be(total - 1);
-            expect(totalInserted).to.be(1);
-
-            db.get('some_key', function (err, res) {
+            db.replace({
+                some_key1: 'has been replaced1',
+                some_key2: 'has been replaced2'
+            }, function (err) {
                 __throw(err);
 
-                expect(res).to.be(valueInserted);
+                var tasks = {};
+
+                tasks.some_key1 = db.get.bind(db, 'some_key1');
+                tasks.some_key2 = db.get.bind(db, 'some_key2');
+
+                async.parallel(tasks, function (err, res) {
+                    expect(res.some_key1).to.be('has been replaced1');
+                    expect(res.some_key2).to.be('has been replaced2');
+                });
 
                 return done();
             });
         });
     });
 
-    it('should fail if single key already exists', function (done) {
-        db.insert({
-            some_key: 'now exists'
-        }, function (err, existing) {
+    it('should fail if single key does not exist', function (done) {
+        db.replace({
+            some_key: 'does not exist'
+        }, function (err, misses) {
             __throw(err);
 
-            expect(existing).to.not.contain('some_key1');
-            expect(existing.length).to.be(0);
+            expect(misses).to.contain('some_key');
+            expect(misses.length).to.be(1);
 
             db.get('some_key', function (err, res) {
-                __throw(err);
+                expect(err).to.be.ok();
+                expect(err.notFound).to.be(true);
 
-                expect(res).to.be('now exists');
-
-                db.insert({
-                    some_key: 'should not insert this'
-                }, function (err, existing) {
-                    __throw(err);
-
-                    expect(existing).to.contain('some_key');
-                    expect(existing.length).to.be(1);
-
-                    return done();
-                });
+                return done();
             });
         });
     });
 
-    it('should fail if multiple keys already exists', function (done) {
-        db.insert({
-            some_key1: 'now exists1',
-            some_key2: 'now exists2',
-        }, function (err, existing) {
+    it('should fail if multiple keys do not exist', function (done) {
+        db.replace({
+            some_key1: 'does not exist1',
+            some_key2: 'does not exist2',
+        }, function (err, misses) {
             __throw(err);
 
-            expect(existing).to.not.contain('some_key1');
-            expect(existing).to.not.contain('some_key2');
-            expect(existing.length).to.be(0);
+            expect(misses).to.contain('some_key1');
+            expect(misses).to.contain('some_key2');
+            expect(misses.length).to.be(2);
 
-            var tasks = {};
-
-            tasks.some_key1 = db.get.bind(db, 'some_key1');
-            tasks.some_key2 = db.get.bind(db, 'some_key2');
-
-            async.parallel(tasks, function (err, res) {
-                __throw(err);
-
-                expect(res.some_key1).to.be('now exists1');
-                expect(res.some_key2).to.be('now exists2');
-
-                db.insert({
-                    some_key1: 'should not insert 1',
-                    some_key2: 'should not insert 2',
-                }, function (err, existing) {
-                    __throw(err);
-
-                    expect(existing).to.contain('some_key1');
-                    expect(existing).to.contain('some_key2');
-                    expect(existing.length).to.be(2);
-
-                    return done();
-                });
-            });
-        });
-    });
-
-    it('should fail if some key in multiple keys already exists', function (done) {
-        db.insert({
-            some_key1: 'now exists1'
-        }, function (err, existing) {
-            __throw(err);
-
-            expect(existing).to.not.contain('some_key1');
-            expect(existing.length).to.be(0);
 
             db.get('some_key1', function (err, res) {
-                __throw(err);
+                expect(err).to.be.ok();
 
-                expect(res).to.be('now exists1');
+                expect(err.notFound).to.be(true);
 
-                db.insert({
-                    some_key1: 'should not insert 1',
-                    some_key2: 'should not insert 2',
-                }, function (err, existing) {
-                    __throw(err);
+                db.get('some_key2', function (err, res) {
+                    expect(err).to.be.ok();
 
-                    expect(existing).to.contain('some_key1');
-                    expect(existing).to.not.contain('some_key2');
-                    expect(existing.length).to.be(1);
+                    expect(err.notFound).to.be(true);
+                    expect(err.notFound).to.be(true);
 
                     return done();
+
+                });
+            });
+        });
+    });
+
+    it('should fail if some key in multiple keys does not exist', function (done) {
+        db.insert({
+            some_key1: 'now exists1',
+            some_key2: 'now exists2'
+        }, function (err, misses) {
+            __throw(err);
+
+            db.replace({
+                some_key1: 'has been replaced1',
+                some_key2: 'has been replaced2',
+                some_key3: 'should be replaced3'
+            }, function (err, misses) {
+                __throw(err);
+
+                expect(misses).to.not.contain('some_key1');
+                expect(misses).to.not.contain('some_key2');
+                expect(misses).to.contain('some_key3');
+
+                db.get('some_key1', function (err, res) {
+                    __throw(err);
+
+                    expect(res).to.be('has been replaced1');
+
+                    db.get('some_key2', function (err, res) {
+                        __throw(err);
+
+                        expect(res).to.be('has been replaced2');
+
+                        db.get('some_key3', function (err, res) {
+                            expect(err).to.be.ok();
+                            expect(err.notFound).to.be(true);
+
+                            return done();
+                        });
+                    });
                 });
             });
         });
